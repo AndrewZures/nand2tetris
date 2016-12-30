1b0f2366e252e872db655c3ada59d30b40951d14
Andrew Zures <andrew.zures@gmail.com>
2016-12-29 18:36:43 -0600

    fill.asm works

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,60 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@i
+M=0
+@COLOR
+M=0
+@8192
+D=A
+@MAX
+M=D
+
+(LOOP)
+@KBD
+D=M
+@BLACK
+D;JGT
+@WHITE
+0;JMP
+
+(BLACK)
+@COLOR
+M=-1
+@FILL
+0;JMP
+
+(WHITE)
+@COLOR
+M=0
+@FILL
+0;JMP
+
+(FILL)
+// load screen address, set to black
+@i
+D=M
+@SCREEN
+D=A+D
+@R5
+M=D
+@COLOR
+D=M
+@R5
+A=M
+M=D
+
+//increment i
+@i
+MD=M+1
+
+//check counter
+@MAX
+D=M-D
+@FILL
+D;JGT
+
+@i
+M=0
+@LOOP
+0;JMP
